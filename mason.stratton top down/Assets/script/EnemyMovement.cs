6f6b3ffb4f807374fad9f4630d03638d280ab9fd
Mason117123 <82406044+Mason117123@users.noreply.github.com>
2021-04-12 11:08:49 -0700

    uploading project

---
+++ mason.stratton top down/Assets/script/EnemyMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float movementspeed = 5;
+    public float detectionRadius = 5;
+    public bool canMove = false;
+    private bool movementDirection = false;
+    public bool isFollowing = false;
+
+    private Rigidbody2D myRB;
+    private CircleCollider2D detectionZone;
+    private Vector2 up;
+    private Vector2 down;
+    private Vector2 zero;
+    public Transform playerTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        up = new Vector2(0, movementspeed);
+        down = new Vector2(0, -movementspeed);
+        zero = new Vector2(0, 0);
+
+        playerTarget = GameObject.Find("playersprite").transform;
+
+        myRB = GetComponent<Rigidbody2D>();
+        detectionZone = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        detectionZone.radius = detectionRadius;
+
+        if (isFollowing == false)
+            myRB.velocity = zero;
+       
+        else if (isFollowing == true)
+        {
+            Vector3 lookPos = playerTarget.position - transform.position;
+            float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+            myRB.rotation = angle;
+            lookPos.Normalize();
+
+            myRB.MovePosition(transform.position + (lookPos * movementspeed * Time.deltaTime));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("bullit"))
+        {
+            Destroy(collision.gameObject);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("player"))
+            isFollowing = true;
+
+        if (collision.gameObject.name == "trigger1")
+            movementDirection = false;
+
+        else if(collision.gameObject.name == "trigger2")
+            movementDirection = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("player"))
+            isFollowing = false;
+    }
+}
---
+++ mason.stratton top down/Assets/script/playerControl.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    private Rigidbody2D myRB;
+    public float speed = 10;
+    public float bullitspeed = 15;
+    public float bullitLifespam = 1;
+    public int playerHealth = 3;
+    public GameObject bullit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        playerHealth = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerHealth <=0)
+        {
+            transform.SetPositionAndRotation(new Vector2(), new Quaternion());
+            playerHealth = 3;
+        }
+
+        Vector2 velocity = myRB.velocity;
+
+        velocity.x = Input.GetAxisRaw("Horizontal") * speed;
+        velocity.y = Input.GetAxisRaw("Vertical") * speed;
+
+        myRB.velocity = velocity;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GameObject b = Instantiate(bullit, new Vector2(transform.position.x, transform.position.y + 1), transform.rotation);
+            b.GetComponent<Rigidbody2D>().velocity = new Vector2(0, bullitspeed);
+            Destroy(b, bullitLifespam);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            GameObject b = Instantiate(bullit, new Vector2(transform.position.x, transform.position.y - 1), transform.rotation);
+            b.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -bullitspeed);
+            Destroy(b, bullitLifespam);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            GameObject b = Instantiate(bullit, new Vector2(transform.position.x - 1, transform.position.y), transform.rotation);
+            b.GetComponent<Rigidbody2D>().velocity = new Vector2(-bullitspeed, 0);
+            Destroy(b, bullitLifespam);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            GameObject b = Instantiate(bullit, new Vector2(transform.position.x + 1, transform.position.y), transform.rotation);
+            b.GetComponent<Rigidbody2D>().velocity = new Vector2(bullitspeed, 0);
+            Destroy(b, bullitLifespam);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "enemysprite")
+        {
+            playerHealth--;
+        }
+
+        else if((collision.gameObject.name == "pickup") && (playerHealth < 3))
+        {
+            playerHealth++;
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "enemytrigger")
+        {
+            GameObject.Find("enemysprite").GetComponent<EnemyMovement>().canMove = true;
+            GameObject.Find("enemysprite").GetComponent<Rigidbody2D>().constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
